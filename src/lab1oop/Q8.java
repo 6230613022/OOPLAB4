6038a50e0adc9ce1adfc30e6fecf2ad11c20f78a
VOVOBOOK <VOVOBOOK@LAPTOP-9DDE1DKD>
2020-09-16 16:26:56 +0700

    NEW

--- src/lab1oop/Q8.java
+++ src/lab1oop/Q8.java
@@ -3,12 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab1oop;
+package lab1.oop;
 
 /**
  *
  * @author coc
  */
+ import java.util.Scanner;
 public class Q8 {
-    
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    System.out.print("n = ");
+    int n = kb.nextInt();
+    int k = 2;                  
+    while (true) {
+      if (k >= n) break;        
+      if ((n % k) == 0) break;  
+      k++;
+    }
+    System.out.println(k == n); 
+  }
+ 
 }
